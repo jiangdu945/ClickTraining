3a22abe7705953de01860b77dc582a9b2a47f412
jiangdu945 <jiangdu945@hotmail.com>
2019-01-02 17:58:54 +0800

    Add files via upload

---
+++ ClickTraining/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ClickTraining
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.status_currentLevel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.status_combo = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tsmi_speed1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_speed2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_speed3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_level1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_level2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_level3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_level4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.ContextMenuStrip = this.contextMenuStrip1;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1007, 569);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmi_level1,
+            this.tsmi_level2,
+            this.tsmi_level3,
+            this.tsmi_level4});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 92);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.status_currentLevel,
+            this.toolStripStatusLabel2,
+            this.status_combo,
+            this.toolStripDropDownButton1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 546);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1007, 23);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(68, 18);
+            this.toolStripStatusLabel1.Text = "当前等级：";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // status_currentLevel
+            // 
+            this.status_currentLevel.Name = "status_currentLevel";
+            this.status_currentLevel.Size = new System.Drawing.Size(131, 18);
+            this.status_currentLevel.Text = "toolStripStatusLabel2";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(44, 18);
+            this.toolStripStatusLabel2.Text = "连击：";
+            // 
+            // status_combo
+            // 
+            this.status_combo.Name = "status_combo";
+            this.status_combo.Size = new System.Drawing.Size(15, 18);
+            this.status_combo.Text = "0";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmi_speed3,
+            this.tsmi_speed2,
+            this.tsmi_speed1});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(69, 21);
+            this.toolStripDropDownButton1.Text = "速度切换";
+            // 
+            // tsmi_speed1
+            // 
+            this.tsmi_speed1.Name = "tsmi_speed1";
+            this.tsmi_speed1.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_speed1.Text = "慢速";
+            this.tsmi_speed1.Click += new System.EventHandler(this.tsmi_speed1_Click);
+            // 
+            // tsmi_speed2
+            // 
+            this.tsmi_speed2.Name = "tsmi_speed2";
+            this.tsmi_speed2.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_speed2.Text = "普通";
+            this.tsmi_speed2.Click += new System.EventHandler(this.tsmi_speed2_Click);
+            // 
+            // tsmi_speed3
+            // 
+            this.tsmi_speed3.Name = "tsmi_speed3";
+            this.tsmi_speed3.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_speed3.Text = "快速";
+            this.tsmi_speed3.Click += new System.EventHandler(this.tsmi_speed3_Click);
+            // 
+            // tsmi_level1
+            // 
+            this.tsmi_level1.Name = "tsmi_level1";
+            this.tsmi_level1.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_level1.Text = "初级";
+            this.tsmi_level1.Click += new System.EventHandler(this.tsmi_level1_Click);
+            // 
+            // tsmi_level2
+            // 
+            this.tsmi_level2.Name = "tsmi_level2";
+            this.tsmi_level2.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_level2.Text = "中级";
+            this.tsmi_level2.Click += new System.EventHandler(this.tsmi_level2_Click);
+            // 
+            // tsmi_level3
+            // 
+            this.tsmi_level3.Name = "tsmi_level3";
+            this.tsmi_level3.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_level3.Text = "高级";
+            this.tsmi_level3.Click += new System.EventHandler(this.tsmi_level3_Click);
+            // 
+            // tsmi_level4
+            // 
+            this.tsmi_level4.Name = "tsmi_level4";
+            this.tsmi_level4.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_level4.Text = "专家";
+            this.tsmi_level4.Click += new System.EventHandler(this.tsmi_level4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 569);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CF练习器";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel status_currentLevel;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel status_combo;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_speed3;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_speed2;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_speed1;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_level1;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_level2;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_level3;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_level4;
+    }
+}
+
---
+++ ClickTraining/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickTraining
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 等级
+        /// 控制每行显示的方块数
+        /// 等级1：每行10个
+        /// 等级2：每行15
+        /// 等级3：每行21
+        /// 等级4：每行28
+        /// </summary>
+        private static int level = 1;
+        /// <summary>
+        /// 速度
+        /// 控制方块
+        /// 等级1：1秒
+        /// 等级2：0.7秒
+        /// 等级3：0.4秒
+        /// </summary>
+        private static int speed = 1;
+        /// <summary>
+        /// 当前激活方块编码
+        /// </summary>
+        private static int current = 0;
+        /// <summary>
+        /// 显示点击框数量
+        /// </summary>
+        private static int boxNum { get; set; }
+        /// <summary>
+        /// 连击次数
+        /// </summary>
+        private static int combo { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitClickBox();
+            InitSpeed();
+            InitLevelStatus();
+        }
+
+        private void InitClickBox()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            int width = flowLayoutPanel1.Width;
+            int boxwidth = 0;
+
+            switch (level)
+            {
+                case 1:
+                    boxNum = 10;
+                    boxwidth = width / 10;
+                    break;
+                case 2:
+                    boxNum = 15;
+                    boxwidth = width / 15;
+                    break;
+                case 3:
+                    boxNum = 21;
+                    boxwidth = width / 21;
+                    break;
+                case 4:
+                    boxNum = 28;
+                    boxwidth = width / 28;
+                    break;
+            }
+
+            int rows = flowLayoutPanel1.Height / boxwidth;
+            boxNum = boxNum * rows;
+
+            for (int i = 1; i <= boxNum; i++)
+            {
+                PictureBox p1 = new PictureBox();
+                p1.Height = boxwidth;
+                p1.Width = boxwidth;
+                //p1.BackColor = Color.Chocolate;
+                p1.BorderStyle = BorderStyle.FixedSingle;
+                p1.AutoSize = false;
+                p1.Margin = new Padding(0);
+                p1.Name = "PictureBox" + i;
+                p1.Click += P1_Click;
+
+                flowLayoutPanel1.Controls.Add(p1);
+            }
+        }
+
+        private void InitSpeed()
+        {
+            this.timer1.Stop();
+            switch (speed)
+            {
+                case 1:
+                    this.timer1.Interval = 1000;
+                    break;
+                case 2:
+                    this.timer1.Interval = 700;
+                    break;
+                case 3:
+                    this.timer1.Interval = 400;
+                    break;
+            }
+
+            this.timer1.Start();
+        }
+
+        private void InitLevelStatus()
+        {
+            string status = "";
+
+            switch (level)
+            {
+                case 1:
+                    status = "初级";
+                    break;
+                case 2:
+                    status = "中级";
+                    break;
+                case 3:
+                    status = "高级";
+                    break;
+                case 4:
+                    status = "专家";
+                    break;
+            }
+
+            switch (speed)
+            {
+                case 1:
+                    status += " 慢速";
+                    break;
+                case 2:
+                    status += " 普通";
+                    break;
+                case 3:
+                    status += " 快速";
+                    break;
+            }
+
+            status_currentLevel.Text = status;
+        }
+
+        private void P1_Click(object sender, EventArgs e)
+        {
+            if (current != 0 && ((MouseEventArgs)e).Button == MouseButtons.Left)
+            {
+                PictureBox pic = (PictureBox)sender;
+
+                string strNum = pic.Name.Replace("PictureBox", "");
+                int num = Convert.ToInt32(strNum);
+
+                if (num == current)
+                {
+                    combo += 1;
+                }
+                else
+                {
+                    combo = 0;
+                }
+
+                status_combo.Text = combo + "";
+            }
+            else if (((MouseEventArgs)e).Button == MouseButtons.Right)
+            {
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (current != 0)
+            {
+                Control controlBefore = Controls.Find("PictureBox" + current, true)[0];
+                PictureBox picBefore = (PictureBox)controlBefore;
+                picBefore.BackColor = Color.Transparent;
+            }
+
+            current = new Random().Next(1, boxNum);
+
+            Control control = Controls.Find("PictureBox" + current, true)[0];
+            PictureBox pic = (PictureBox)control;
+            pic.BackColor = Color.Wheat;
+        }
+
+        #region 速度
+
+        /// <summary>
+        /// 慢速
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmi_speed1_Click(object sender, EventArgs e)
+        {
+            speed = 1;
+            InitSpeed();
+            InitLevelStatus();
+        }
+
+        /// <summary>
+        /// 普通速度
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmi_speed2_Click(object sender, EventArgs e)
+        {
+            speed = 2;
+            InitSpeed();
+            InitLevelStatus();
+        }
+
+        /// <summary>
+        /// 快熟
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmi_speed3_Click(object sender, EventArgs e)
+        {
+            speed = 3;
+            InitSpeed();
+            InitLevelStatus();
+        }
+
+        #endregion
+
+        #region 等级
+
+        private void tsmi_level1_Click(object sender, EventArgs e)
+        {
+            level = 1;
+            InitClickBox();
+            InitLevelStatus();
+        }
+
+        private void tsmi_level2_Click(object sender, EventArgs e)
+        {
+            level = 2;
+            InitClickBox();
+            InitLevelStatus();
+        }
+
+        private void tsmi_level3_Click(object sender, EventArgs e)
+        {
+            level = 3;
+            InitClickBox();
+            InitLevelStatus();
+        }
+
+        private void tsmi_level4_Click(object sender, EventArgs e)
+        {
+            level = 4;
+            InitClickBox();
+            InitLevelStatus();
+        }
+
+        #endregion
+
+    }
+}
